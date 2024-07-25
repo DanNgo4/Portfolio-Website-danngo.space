2c372ef9fb092557e702223091a638dfd36202a4
Dan Ngo <danngo.au@gmail.com>
2024-07-25 15:34:48 +1000

    deploy render v1.3

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+/* import { MongoClient } from "mongodb";
 
 let db;
 
@@ -7,6 +7,34 @@ async function connectToDB(cb) {    // cb = callback
     await client.connect();
     db = client.db("danngo-space-db");
     cb();
+}
+
+export {
+    db,
+    connectToDB,
+}; */
+
+import { MongoClient } from "mongodb";
+
+let db;
+
+async function connectToDB(cb) {    // cb = callback
+    try {
+        const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@danngo-space-db.kacumve.mongodb.net/danngo-space-db?retryWrites=true&w=majority&ssl=true`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            ssl: true,
+            sslValidate: true,
+            tlsAllowInvalidCertificates: false,
+        });
+        await client.connect();
+        db = client.db("danngo-space-db");
+        console.log("Successfully connected to the database!");
+        cb();
+    } catch (err) {
+        console.error("Failed to connect to the database", err);
+        process.exit(1); // Exit the process with a failure code
+    }
 }
 
 export {
