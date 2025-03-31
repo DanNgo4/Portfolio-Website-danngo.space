3577dd8fa92fcc2d1db3f5cdc1fc5f32c26cd421
Dan Ngo <danngo.au@gmail.com>
2025-03-31 21:55:24 +1100

    Delete App.test.js

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders about link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/about/i);
-  expect(linkElement).toBeInTheDocument();
-});
